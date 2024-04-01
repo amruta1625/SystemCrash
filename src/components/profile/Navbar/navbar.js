2a3786327294dc221eb84bf40bda208b9a2f95a0
amruta1625 <swecha623@gmail.com>
2024-04-01 22:36:36 +0530

    menu

--- src/components/profile/Navbar/navbar.js
+++ src/components/profile/Navbar/navbar.js
@@ -3,6 +3,7 @@ import './navbar.css';
 import { Link, useNavigate } from 'react-router-dom';
 import logotradethrill from '../../../logotradethrill.svg';
 import AuthContext from '../../../context/AuthProvider';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Navbar(props) {
   const navigate = useNavigate();
@@ -35,17 +36,23 @@ export default function Navbar(props) {
     setMenuOpen(!menuOpen);
   };
 
+  const handleMenuItemClick = (route) => {
+    setMenuOpen(false);
+    navigate(route);
+  };
+
   return (
     <>
       <nav>
         <input type="checkbox" id="check" checked={menuOpen} onChange={toggleMenu} />
         <label htmlFor="check" className="checkbtn">
-          <i className="fas fa-bars" onClick={toggleMenu}></i>
+          <MenuIcon className="menu-icon" />
         </label>
 
         <img className="logopic" onClick={goToHome} src={logotradethrill} alt="TradeThrill" />
 
         <label className="logo" onClick={goToHome}>TradeThrill</label>
+        
         <ul className={menuOpen ? "navbar-menu active" : "navbar-menu"}>
           <li>
             <Link className={props.vp} to="/profilepage">
@@ -65,6 +72,17 @@ export default function Navbar(props) {
           </li>
         </ul>
       </nav>
+
+      {menuOpen && (
+        <div className="dropdown-menu">
+          <ul>
+            <li onClick={() => handleMenuItemClick("/profilepage")}>View Profile</li>
+            <li onClick={() => handleMenuItemClick("/transactions")}>Transactions</li>
+            <li onClick={() => handleMenuItemClick("/changepassword")}>Change Password</li>
+            <li onClick={() => handleMenuItemClick("/logout")}>Logout</li>
+          </ul>
+        </div>
+      )}
     </>
   );
 }
