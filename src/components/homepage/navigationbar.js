c6a7fa77712fb26ad666002e9a6ac3805b432885
amruta1625 <swecha623@gmail.com>
2024-04-01 21:25:14 +0530

    navbar

--- src/components/homepage/navigationbar.js
+++ src/components/homepage/navigationbar.js
@@ -1,112 +1,3 @@
-// import React, { useState } from "react";
-// import logotradethrill from "../../logotradethrill.svg";
-// import NotificationsIcon from "@mui/icons-material/NotificationsNoneRounded";
-// import ProfileIcon from "@mui/icons-material/AccountCircleRounded";
-// import InputBase from "@mui/material/InputBase";
-// import InputAdornment from "@mui/material/InputAdornment";
-// import SearchIcon from "@mui/icons-material/Search";
-// import { useNavigate } from "react-router-dom";
-// import "./navigationbar.css";
-// import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-// import axios from "axios";
-// import StorefrontIcon from '@mui/icons-material/Storefront';
-
-// const Navbar = ({ search_stuff }) => {
-//   const { products, setProducts } = search_stuff;
-//   const navigate = useNavigate();
-//   const [searchString, setSearchString] = useState("");
-
-//   const goToSellPage = () => {
-//     navigate("/sellpage");
-//   };
-
-//   const goToChatPage = () => {
-//     navigate("/chatpage");
-//   };
-
-//   const goToNotificationPage = () => {
-//     navigate("/notify");
-//   };
-
-//   const goToProfilePage = () => {
-//     navigate("/profilepage");
-//   };
-
-//   const goToWishlist = () => {
-//     navigate("/wishlist"); // Assuming your wishlist page is at this route
-//   };
-
-//   const goToUploadedItems = () => {
-//     navigate("/uploadeditems");
-//   };
-
-//   const handleSearch = (e) => {
-//     e.preventDefault();
-//     if (searchString.trim() === "") {
-//       axios.get("https://elan.iith-ac.in:8082/get_products")
-//         .then((res) => {
-//           setProducts(res.data);
-//         })
-//         .catch((err) => {
-//           console.log(err);
-//         });
-//     } else {
-//       axios.post("https://elan.iith-ac.in:8082/search", { query: searchString })
-//         .then((res) => {
-//           setProducts(res.data);
-//         })
-//         .catch((err) => {
-//           console.log(err);
-//         });
-//     }
-//   };
-
-//   const handleKeyPress = (e) => {
-//     if (e.key === 'Enter') {
-//       handleSearch(e);
-//     }
-//   };
-
-//   return (
-//     <div className="navbar">
-//       <img src={logotradethrill} alt="logo" className="navbar-logo" />
-//       <div className="navbar-logo-name">TradeThrill</div>
-//       <div className="search-container">
-//         <InputBase
-//           className="searchbar"
-//           placeholder="Search for items"
-//           value={searchString}
-//           onChange={(e) => setSearchString(e.target.value)}
-//           onKeyPress={handleKeyPress} // Bind handleKeyPress function to onKeyPress event
-//           endAdornment={
-//             <InputAdornment position="end">
-//               <SearchIcon
-//                 className="search-icon"
-//                 onClick={(e) => handleSearch(e)}
-//               />
-//             </InputAdornment>
-//           }
-//         />
-//       </div>
-//       <FavoriteBorderIcon className="favoriteicon" onClick={goToWishlist} />
-//       <StorefrontIcon className="uploadedicon" onClick={goToUploadedItems} />
-//       <button className="navbar-button" onClick={goToSellPage} >
-//         SELL
-//       </button>
-//       <button className="navbar-button" onClick={goToChatPage}>
-//         CHAT
-//       </button>
-//       <NotificationsIcon
-//         className="notificationicon"
-//         onClick={goToNotificationPage}
-//       />
-//       <ProfileIcon className="profileicon" onClick={goToProfilePage} />
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-
 import React, { useState } from "react";
 import logotradethrill from "../../logotradethrill.svg";
 import NotificationsIcon from "@mui/icons-material/NotificationsNoneRounded";
@@ -121,12 +12,15 @@ import axios from "axios";
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Tooltip } from "@mui/material";
+import { useMediaQuery } from '@mui/material';
 
 const Navbar = ({ search_stuff }) => {
   const { products, setProducts } = search_stuff;
   const navigate = useNavigate();
   const [searchString, setSearchString] = useState("");
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const isDesktop = useMediaQuery('(min-width:768px)');
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
@@ -149,7 +43,6 @@ const Navbar = ({ search_stuff }) => {
     e.preventDefault();
     if (searchString.trim() === "") {
       axios.get("https://elan.iith-ac.in:8082/get_products")
-      // axios.get("http://127.0.0.1:8000/get_products")
       .then((res) => {
           setProducts(res.data);
         })
@@ -158,7 +51,6 @@ const Navbar = ({ search_stuff }) => {
         });
     } else {
       axios.post("https://elan.iith-ac.in:8082/search", { query: searchString })
-      // axios.post("http://127.0.0.1:8000/search", { query: searchString })
         .then((res) => {
           setProducts(res.data);
         })
@@ -197,31 +89,44 @@ const Navbar = ({ search_stuff }) => {
       </div>
       <div className="menu-toggle" onClick={toggleMobileMenu}>
         <MenuIcon className="menu-icon" />
+        {isMobileMenuOpen && (
+          <div className="dropdown-menu">
+            <ul>
+              <li onClick={() => handleMenuItemClick("/profilepage")}>Profile</li>
+              <li onClick={() => handleMenuItemClick("/sellpage")}>Sell Item</li>
+              <li onClick={() => handleMenuItemClick("/chatpage")}>Chat</li>
+              <li onClick={() => handleMenuItemClick("/wishlist")}>Wishlist</li>
+              <li onClick={() => handleMenuItemClick("/uploadeditems")}>Uploaded Items</li>
+              <li onClick={() => handleMenuItemClick("/notify")}>Notifications</li>
+            </ul>
+          </div>
+        )}
       </div>
-      <div className={`navigation-items ${isMobileMenuOpen ? 'hidden' : ''}`}>
-        {/* Render navigation items here */}
-        <Tooltip title="Wishlist" placement="bottom">
-        <FavoriteBorderIcon className="favoriteicon" onClick={() => handleMenuItemClick("/wishlist")} />
-        </Tooltip>
-        <Tooltip title="Uploaded Items" placement="bottom">
-        <StorefrontIcon className="uploadedicon" onClick={() => handleMenuItemClick("/uploadeditems")} />
-        </Tooltip>
-        <button className="navbar-button" onClick={() => handleMenuItemClick("/sellpage")} >
-          SELL
-        </button>
-        <button className="navbar-button" onClick={() => handleMenuItemClick("/chatpage")}>
-          CHAT
-        </button>
-        <Tooltip title="Notifications" placement="bottom">
-        <NotificationsIcon
-          className="notificationicon"
-          onClick={() => handleMenuItemClick("/notify")}
-        />
-        </Tooltip>
-        <Tooltip title="Profile" placement="bottom">
-        <ProfileIcon className="profileicon" onClick={() => handleMenuItemClick("/profilepage")} />
-        </Tooltip>
-      </div>
+      {isDesktop && (
+        <div className="desktop-icons">
+          <Tooltip title="Wishlist" placement="bottom">
+            <FavoriteBorderIcon className="favoriteicon" onClick={() => handleMenuItemClick("/wishlist")} />
+          </Tooltip>
+          <Tooltip title="Products on Sale" placement="bottom">
+            <StorefrontIcon className="uploadedicon" onClick={() => handleMenuItemClick("/uploadeditems")} />
+          </Tooltip>
+          <button className="navbar-button" onClick={() => handleMenuItemClick("/sellpage")} >
+            SELL
+          </button>
+          <button className="navbar-button" onClick={() => handleMenuItemClick("/chatpage")}>
+            CHAT
+          </button>
+          <Tooltip title="Notifications" placement="bottom">
+            <NotificationsIcon
+              className="notificationicon"
+              onClick={() => handleMenuItemClick("/notify")}
+            />
+          </Tooltip>
+          <Tooltip title="Profile" placement="bottom">
+            <ProfileIcon className="profileicon" onClick={() => handleMenuItemClick("/profilepage")} />
+          </Tooltip>
+        </div>
+      )}
     </div>
   );
 };
